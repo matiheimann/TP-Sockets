b6f6279fb7292de8febfea47692383cfdb829b82
Matias <mheimann@itba.edu.ar>
2018-06-07 14:11:11 -0300

    First commit

---
+++ server.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+#include <string.h> 
+#include <stdlib.h> 
+#include <errno.h> 
+#include <unistd.h>  
+#include <arpa/inet.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <sys/time.h>
+
+
+int main()
+{
+	int socketFd;
+}
