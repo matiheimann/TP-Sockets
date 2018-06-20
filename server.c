6b39c8e0869821c23bd4b5afbbc41c52cfb55af5
Unknown <lmikolas@itba.edu.ar>
2018-06-19 22:14:30 -0300

    Server

--- server.c
+++ server.c
@@ -8,9 +8,64 @@
 #include <sys/socket.h> 
 #include <netinet/in.h> 
 #include <sys/time.h>
+#include <pthread.h>
+#include "server.h"
+
 
 
 int main()
 {
-	int socketFd;
+	int serverDescriptor;
+	int clientDescriptor;
+	int * connectionDescriptor
+	char message[MESSAGE_SIZE];
+	struct sockaddr_un serverAddress, clientAddress;
+	socklent_t clientAddressSize;
+
+	
+	//New socket
+	serverDescriptor = socket(AF_LOCAL, SOCK_STREAM, 0);
+	if(socket == -1)
+	{
+		perror("Socket could not be created");
+		exit(1)
+	}
+	memset(&serverAddress, 0, sizeof(struct sockaddr_un));
+	strncpy(serverAddress.sun_path, SOCKET_PATH, sizeof(serverAddress.sun_path));
+
+	//Give name to socket
+	if(bind(serverDescriptor, (struct sockaddr *) &serverAddress, sizeof(struct sockaddr_un)) == -1)
+	{
+		perror("Bind error");
+		exit(1);
+	}
+
+	if(listen(serverDescriptor, LISTEN_BACKLOG) == -1)
+	{
+		perror("Listen error");
+		exit(1);
+	}
+	while(clientDescriptor = accept(serverDescriptor,(struct sockaddr*)&clientAddress, &clientAddressSize))
+	{
+		pthread_t thread;
+		connectionDescriptor = malloc(sizeof(int));
+		*connectionDescriptor = clientDescriptor;
+		if(pthread_create(&thread, NULL, connectionSolver, (void*)connectionDescriptor) == -1)
+		{
+			perror("Thread creation error");
+			exit(1);
+		}
+	}
+
+	if(clientDescriptor = -1)
+	{
+		perror("Accept error");
+		exit(1);
+	}
+	
+}
+
+void connectionSolver(void *connectionDescriptor)
+{
+	/*Aca se resuelve la conexion con el servidor*/
 }
---
+++ server.h
@@ -0,0 +1,8 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#define MESSAGE_SIZE 2000
+#define SOCKET_PATH "/path"
+#define LISTEN_BACKLOG 50
+void connectionSolver(void *connectionDescriptor);
+#endif
