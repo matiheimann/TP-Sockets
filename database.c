52b3b63fc7c304d4394877e172085a03b7259b70
jkatan <jkatan@itba.edu.ar>
2018-06-23 17:29:44 -0300

    adding database interface library

--- database.c
+++ database.c
@@ -1,12 +1,28 @@
 #include "include/database.h"
 #include <string.h>
-#include <sqlite3.h> 
 #include <stdio.h>
 
 //Simple test
 int main()
 {
-	addUser("Lorenord", 96);
+	sqlite3* database;
+
+	if(sqlite3_open("database.db", &database) != SQLITE_OK)
+   	{
+   		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(database));
+   	}
+
+   	executeSQLStatement("PRAGMA foreign_keys = ON;", database); //Habilito las FOREIGN KEYS en sqlite3
+
+   	addFlight(database, 1);
+   	addUser(database, "Lorenord", 96);
+   	addSeat(database, 10, "available", 1);
+   	addReservation(database, 96, 10, 1);
+
+   	deleteFlight(database, 1); //Al estar ON DELETE CASCADE para las foreign keys, se eliminaran los asientos 
+   	  							//y reservas asociados al vuelo eliminado.
+
+	sqlite3_close(database);
 }
 
 char* handleQuery(char* query)
@@ -14,35 +30,133 @@ char* handleQuery(char* query)
 	return "Hi im server!";
 }
 
-int addUser(char* username, int userID)
+void addUser(sqlite3* database, char* username, int userID)
 {
-   sqlite3 *database;
-   char *errorMessage = 0;
-   char sqlStatement[60];
+   char sqlStatement[100];
    char userIDToString[5]; 
-   int connectionReturnValue = sqlite3_open("database.db", &database);
 
-   if(connectionReturnValue != SQLITE_OK)
-   {
-   		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(database));
-   }
-
-   strcpy(sqlStatement,"INSERT INTO user(userID, username) VALUES(");
+   strcpy(sqlStatement,"INSERT INTO user VALUES(");
    sprintf(userIDToString, "%d", userID);
    strcat(sqlStatement, userIDToString);
    strcat(sqlStatement, ",'");
    strcat(sqlStatement, username);
    strcat(sqlStatement, "');");
 
-   connectionReturnValue = sqlite3_exec(database, sqlStatement, NULL, 0, &errorMessage);
+   executeSQLStatement(sqlStatement, database);
+}
 
-   if( connectionReturnValue != SQLITE_OK ){
-      fprintf(stderr, "SQL error: %s\n", errorMessage);
-      sqlite3_free(errorMessage);
-  }
+void deleteUser(sqlite3* database, int userID)
+{
+	char sqlStatement[60];
+	char userIDToString[5]; 
 
-   printf("%s\n", sqlStatement);
+	strcpy(sqlStatement,"DELETE FROM user WHERE userID=");
+	sprintf(userIDToString, "%d", userID);
+	strcat(sqlStatement, userIDToString);
+	strcat(sqlStatement, ";");
 
+	executeSQLStatement(sqlStatement, database);
+}
 
-  sqlite3_close(database);
+void addSeat(sqlite3* database, int seatNumber, char* state, int flightNumber)
+{
+	char sqlStatement[100];
+	char seatNumberToString[2]; 
+	char flightNumberToString[3];
+
+	strcpy(sqlStatement,"INSERT INTO seat VALUES(");
+	sprintf(seatNumberToString, "%d", seatNumber);
+	strcat(sqlStatement, seatNumberToString);
+	strcat(sqlStatement, ",'");
+	strcat(sqlStatement, state);
+	strcat(sqlStatement, "',");
+	sprintf(flightNumberToString, "%d", flightNumber);
+	strcat(sqlStatement, flightNumberToString);
+	strcat(sqlStatement, ");");
+
+	executeSQLStatement(sqlStatement, database);
+}
+
+void deleteSeat(sqlite3* database, int seatNumber)
+{
+	char sqlStatement[100];
+	char seatNumberToString[2]; 
+
+	strcpy(sqlStatement,"DELETE FROM seat WHERE seatNumber=");
+	sprintf(seatNumberToString, "%d", seatNumber);
+	strcat(sqlStatement, seatNumberToString);
+	strcat(sqlStatement, ";");
+
+	executeSQLStatement(sqlStatement, database);
+}
+
+void addFlight(sqlite3* database, int flightNumber)
+{
+	char sqlStatement[50];
+	char flightNumberToString[3];
+
+	strcpy(sqlStatement,"INSERT INTO flight VALUES(");
+	sprintf(flightNumberToString, "%d", flightNumber);
+	strcat(sqlStatement, flightNumberToString);
+	strcat(sqlStatement, ");");
+
+	executeSQLStatement(sqlStatement, database);
+}
+
+void deleteFlight(sqlite3* database, int flightNumber)
+{
+	char sqlStatement[50];
+	char flightNumberToString[3];
+
+	strcpy(sqlStatement,"DELETE FROM flight WHERE flightID=");
+	sprintf(flightNumberToString, "%d", flightNumber);
+	strcat(sqlStatement, flightNumberToString);
+	strcat(sqlStatement, ";");
+
+	executeSQLStatement(sqlStatement, database);
+}
+
+void addReservation(sqlite3* database, int userID, int seatNumber, int flightNumber)
+{
+	char sqlStatement[50];
+	char flightNumberToString[3];
+	char seatNumberToString[2];
+	char userIDToString[5];
+
+	strcpy(sqlStatement,"INSERT INTO reservation VALUES(");
+	sprintf(userIDToString, "%d", userID);
+	strcat(sqlStatement, userIDToString);
+	strcat(sqlStatement, ",");
+	sprintf(seatNumberToString, "%d", seatNumber);
+	strcat(sqlStatement, seatNumberToString);
+	strcat(sqlStatement, ",");
+	sprintf(flightNumberToString, "%d", flightNumber);
+	strcat(sqlStatement, flightNumberToString);
+	strcat(sqlStatement, ");");
+
+	executeSQLStatement(sqlStatement, database);
+}
+
+void deleteReservation(sqlite3* database, int userID)
+{
+	char sqlStatement[50];
+	char userIDToString[5];
+
+	strcpy(sqlStatement,"DELETE FROM reservation WHERE userID=");
+	sprintf(userIDToString, "%d", userID);
+	strcat(sqlStatement, userIDToString);
+	strcat(sqlStatement, ";");
+
+	executeSQLStatement(sqlStatement, database);
+}
+
+void executeSQLStatement(char* statement, sqlite3* database)
+{
+	char *errorMessage = 0;
+
+	if(sqlite3_exec(database, statement, NULL, 0, &errorMessage) != SQLITE_OK)
+	{
+		fprintf(stderr, "SQL error: %s\n", errorMessage);
+      	sqlite3_free(errorMessage);
+	}
 }
--- include/database.h
+++ include/database.h
@@ -1,6 +1,19 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
+#include <sqlite3.h> 
+
 char* handleQuery(char* query);
-int addUser(char* username, int userID);
+void addUser(sqlite3* database, char* username, int userID);
+void deleteUser(sqlite3* database, int userID);
+void connectToDatabase(sqlite3* database);
+void disconnectFromDatabase(sqlite3* database);
+void executeSQLStatement(char* statement, sqlite3* database);
+void addSeat(sqlite3* database, int seatNumber, char* state, int flightNumber);
+void deleteSeat(sqlite3* database, int seatNumber);
+void addFlight(sqlite3* database, int flightNumber);
+void deleteFlight(sqlite3* database, int flightNumber);
+void addReservation(sqlite3* database, int useriD, int seatNumber, int flightID);
+void deleteReservation(sqlite3* database, int userID);
+
 #endif
